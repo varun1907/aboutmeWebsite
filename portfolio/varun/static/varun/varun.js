69685679cd50b76f2f5eae0b27fd6d2317de4c07
varun1907 <varun1907@gmail.com>
2017-12-19 01:34:16 +0530

    Smooth Scrolling Added

--- portfolio/varun/static/varun/varun.js
+++ portfolio/varun/static/varun/varun.js
@@ -1,4 +1,66 @@
 
+function doFirst()
+{
+	var ul=document.getElementById('foo');
+  li=ul.getElementsByTagName('li');
+	var x=ul.getElementsByTagName('a');
+
+	x[0].addEventListener('mouseover',function(){
+				li[0].style.backgroundColor='black';
+        x[0].style.textDecoration="none";
+        x[0].style.color="white";
+
+		      });
+	x[0].addEventListener('mouseout',function(){
+				li[0].style.backgroundColor='#6D695E';
+		      });
+
+	x[1].addEventListener('mouseover',function(){
+				li[1].style.backgroundColor='black';
+        x[1].style.textDecoration="none";
+        x[1].style.color="white";
+
+		      });
+	x[1].addEventListener('mouseout',function(){
+				li[1].style.backgroundColor='#6D695E';
+		      });
+
+		x[2].addEventListener('mouseover',function(){
+				li[2].style.backgroundColor='black';
+        x[2].style.textDecoration="none";
+        x[2].style.color="white";
+
+		      });
+	x[2].addEventListener('mouseout',function(){
+				li[2].style.backgroundColor='#6D695E';
+		      });
+
+		x[3].addEventListener('mouseover',function(){
+				li[3].style.backgroundColor='black';
+          x[3].style.textDecoration="none";
+          x[3].style.color="white";
+
+		      });
+	x[3].addEventListener('mouseout',function(){
+				li[3].style.backgroundColor='#6D695E';
+      });
+
+  x[4].addEventListener('mouseover',function(){
+  			li[4].style.backgroundColor='black';
+        x[4].style.textDecoration="none";
+        x[4].style.color="white";
+      });
+	x[4].addEventListener('mouseout',function(){
+			li[4].style.backgroundColor='#6D695E';
+      });
+}
+
+window.addEventListener('load',doFirst,false);
+
+
+
+
+
 var marginY=0;
 var destination=0;
 var speed=8;
